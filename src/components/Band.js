83dab8f776cf265f7b1821d3567fb16350546c95
github username <github email address>
2020-09-13 19:35:54 -0400

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 
 class Band extends Component {
 
+  handleOnClick = () => {
+    this.props.deleteBand(this.props.id)
+  }
+
   render() {
     return(
       <div>
         <li>{this.props.name}</li>
-        <button onClick={this.props.deleteBand}>Delete</button>
+        <button onClick={this.handleOnClick}>Delete</button>
       </div>
     );
   }
--- src/components/Bands.js
+++ src/components/Bands.js
@@ -3,7 +3,7 @@ import Band from './Band'
 
 class Bands extends Component {
 
-  renderBands = () => this.props.bands.map(band => <Band name={band.name} key={band.id} deleteBand={this.props.deleteBand}/>)
+  renderBands = () => this.props.bands.map(band => <Band name={band.name} key={band.id} id={band.id} deleteBand={this.props.deleteBand}/>)
 
   render() {
       console.log(this.props.bands)
