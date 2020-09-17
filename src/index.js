aad7212605ac27db989f97d4940db9e7bf4fe875
HarryWilson1995 <harry.wilson1995a@gmail.com>
2020-09-17 09:07:24 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './App';
-import manageBand from './reducers/manageBand'
+import manageBand from './reducers/manageBand';
 
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
-const store = createStore(manageBand)
-
+const store = createStore(manageBand);
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-document.getElementById('root')
-)
+  document.getElementById('root')
+);
